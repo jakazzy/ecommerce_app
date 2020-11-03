62577a9ca2a10cfea168eea510d287b24b3532c4
jakazzy <jakazzy@gmail.com>
2020-11-03 11:08:33 +0000

    make corrections to the SpecialOffer component for the HomeScreen

--- lib/screens/home/components/special_offer.dart
+++ lib/screens/home/components/special_offer.dart
@@ -12,10 +12,14 @@ class SpecialOffer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SectionTitle(
-          text: "Special for you",
-          press: (){},
+        Padding(
+            padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+            child:   SectionTitle(
+              text: "Special for you",
+              press: (){},
+            ),
         ),
+
         SizedBox( height: getProportionateScreenWidth(20)),
         SingleChildScrollView(
           scrollDirection: Axis.horizontal,
@@ -59,49 +63,53 @@ class SpecialOfferCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(left: getProportionateScreenWidth(20)),
-      child: SizedBox(
-        height: getProportionateScreenWidth(100),
-        width: getProportionateScreenHeight(242),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: Stack(
-            children: [
-              Image.asset(
-                image,
-                fit: BoxFit.cover,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Color(0xFF343434).withOpacity(0.4),
-                          Color(0xFF343434).withOpacity(0.15),
-                        ]
-                    )
+      child: GestureDetector(
+        onTap: press,
+        child: SizedBox(
+          height: getProportionateScreenWidth(100),
+          width: getProportionateScreenWidth(242),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Stack(
+              children: [
+                Image.asset(
+                  image,
+                  fit: BoxFit.cover,
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: getProportionateScreenWidth(15),
-                    vertical: getProportionateScreenWidth(10)),
-                child: Text.rich(
-                    TextSpan(
-                        style: TextStyle( color: Colors.white),
-                        children: [
-                          TextSpan(text: "$category\n",
-                              style: TextStyle(
-                                fontSize: getProportionateScreenWidth(18),
-                                fontWeight: FontWeight.bold,
-                              )
-                          ),
-                          TextSpan(text: "$numOfBrands"),
-                        ]
-                    )
+                Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Color(0xFF343434).withOpacity(0.4),
+                            Color(0xFF343434).withOpacity(0.15),
+                          ]
+                      )
+                  ),
                 ),
-              )
-            ],
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: getProportionateScreenWidth(15),
+                      vertical: getProportionateScreenWidth(10)),
+                  child: Text.rich(
+                      TextSpan(
+                          style: TextStyle( color: Colors.white),
+                          children: [
+                            TextSpan(
+                                text: "$category\n",
+                                style: TextStyle(
+                                  fontSize: getProportionateScreenWidth(18),
+                                  fontWeight: FontWeight.bold,
+                                )
+                            ),
+                            TextSpan(text: "$numOfBrands"),
+                          ]
+                      )
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
