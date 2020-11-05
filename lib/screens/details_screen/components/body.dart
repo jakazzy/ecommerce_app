2e61d6625e9b217d5965d4d0593ccdb6bb135a8e
jakazzy <jakazzy@gmail.com>
2020-11-05 17:40:27 +0000

    add product details screen

--- lib/screens/details_screen/components/body.dart
+++ lib/screens/details_screen/components/body.dart
@@ -1,8 +1,12 @@
-import 'package:ecommerce/constants.dart';
+import 'package:ecommerce/components/default_button.dart';
 import 'package:ecommerce/models/Product.dart';
+import 'package:ecommerce/screens/details_screen/components/product_description.dart';
 import 'package:ecommerce/screens/details_screen/components/product_images.dart';
+import 'package:ecommerce/screens/details_screen/components/top_rounded_container.dart';
 import 'package:ecommerce/size_config.dart';
 import 'package:flutter/material.dart';
+
+import 'color_dots.dart';
 
 class Body extends StatelessWidget {
 
@@ -11,7 +15,49 @@ class Body extends StatelessWidget {
   final Product product;
   @override
   Widget build(BuildContext context) {
-    return ProductImages(product: product);
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ProductImages(product: product),
+          TopRoundedContainer(
+            color: Colors.white,
+            child: Column(
+              children: [
+                ProductDescription(
+                    product: product,
+                    pressOnSeeMore: (){},
+                ),
+                TopRoundedContainer(
+                    color: Color(0xFFF6F7F9),
+                    child: Column(
+                      children: [
+                        ColorDots(product: product),
+                        TopRoundedContainer(
+                            color: Colors.white,
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                  left: SizeConfig.screenWidth * 0.15,
+                                  right: SizeConfig.screenWidth * 0.15,
+                                  top: getProportionateScreenWidth(15),
+                                  bottom: getProportionateScreenWidth(40),
+                              ),
+                              child: DefaultButton(
+                                text: "Add to Cart",
+                                press: (){},
+                              ),
+                            )
+                        )
+                      ],
+                    )
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
+
+
 
